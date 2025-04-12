858658f167453238a460e9c12ddbcc477edc84b8
Gabrielbm2 <reistomm@gmail.com>
2025-04-12 11:51:12 -0300

    feat(setup): Configuring and improving migration files
    - Create route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'auth#login'
+      post '/register', to: 'auth#register'
+      get '/me', to: 'auth#me'
+      
+      resources :users
+      
+      resources :products do
+        member do
+          post 'update_stock', to: 'products#update_stock'
+        end
+      end
+      
+      get '/users/:user_id/products', to: 'products#index'
+      
+      post '/forgot_password', to: 'passwords#forgot'
+      post '/reset_password', to: 'passwords#reset'
+    end
+  end
 end
